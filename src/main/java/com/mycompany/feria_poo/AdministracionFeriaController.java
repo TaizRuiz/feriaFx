025487ec8e23a2385138a79a6f811fa4e060f157
kevinMaga <kerimaga@espol.edu.ec>
2024-01-05 22:29:01 -0500

    imagenes

---
+++ src/main/java/com/mycompany/feria_poo/AdministracionFeriaController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.feria_poo;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author UserPC
+ */
+public class AdministracionFeriaController implements Initializable {
+
+    @FXML
+    private ImageView btregresar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void regresar(MouseEvent event) {
+        try {
+            App.setRoot("primary");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+}
--- src/main/java/com/mycompany/feria_poo/PrimaryController.java
+++ src/main/java/com/mycompany/feria_poo/PrimaryController.java
@@ -2,11 +2,42 @@ package com.mycompany.feria_poo;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
 
     @FXML
+    private ImageView iFeria;
+    @FXML
+    private ImageView iEmpre;
+    @FXML
+    private ImageView iAuspi;
+    @FXML
+    private ImageView iStans;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void administrarFeria(MouseEvent event) {
+        try {
+            App.setRoot("administracionFeria");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void administrarEmprendedores(MouseEvent event) {
+    }
+
+    @FXML
+    private void administradorAuspiciantes(MouseEvent event) {
+    }
+
+    @FXML
+    private void administradorStans(MouseEvent event) {
     }
 }
